74408a2f80eeff30d85e6f78429f1b5453741a90
Admin <kristian@Kristians-MacBook-Pro.local>
2013-07-22 11:52:11 +0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Admin on 7/22/13.
+//  Copyright (c) 2013 VIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Admin on 7/22/13.
+//  Copyright (c) 2013 VIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
